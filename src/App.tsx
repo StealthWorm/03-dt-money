359d47352903a45bf7a216ff8987b65d160eed74
StealthWorm <thierrypitela@hotmail.com>
2023-08-24 20:03:01 -0300

    feat: first commit 03

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <Transactions />
+   </ThemeProvider>
+  )
+}
